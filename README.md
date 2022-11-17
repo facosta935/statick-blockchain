1a1384415b49782b0c2af483462ef8d4611cbbd5
Florentino Daniel Acosta Nava <florentino.acosta@sensanet.com.mx>
2022-11-17 10:45:41 -0600

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# statick-blockchain
+# Static Blockchain
+
+Este proyecto crea una blockchain con datos ya definidos. Es un ejemplo de cómo se crea una blockchain.
+
+Este proyecto fue basado de otro proyecto, posteriormente mostraré la fuente orifinal.
---
+++ index.js
@@ -0,0 +1,25 @@
+const Blockchain = require('./src/blockchain');
+const Block = require('./src/block');
+
+async function run(){
+    const blockchain = await new Blockchain();
+
+    const block1 = new Block({data: "Block #1"});
+    const block2 = new Block({data: "Block #2"});
+    const block3 = new Block({data: "Block #3"});
+    const block4 = new Block({data: "Block #4"});
+    const block5 = new Block({data: "Block #5"});
+    const block6 = new Block({data: "Block #6"});
+
+    await blockchain.addBlock(block1);
+    await blockchain.addBlock(block2);
+    await blockchain.addBlock(block3);
+    await blockchain.addBlock(block4);
+    await blockchain.addBlock(block5);
+    await blockchain.addBlock(block6);
+
+    blockchain.print();
+
+}
+
+run();
